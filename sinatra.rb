d6188df410306b6107275ef3299ddc7886a732b0
silas.xie <xie.tianbao44@gmail.com>
2014-01-01 22:38:10 +0800

    use shotgun to start the webserver, so it load the script eveytime automatically

--- sinatra.rb
+++ sinatra.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
 set :public_folder, File.dirname(__FILE__) + '/public_folder'
-#set :views, File.dirname(__FILE__) + '/templates'
+set :views, File.dirname(__FILE__) + '/views'
 
 get '/' do
     'hello worldddd!'
+    'hhhhhhhhhhhhhhhhhhhhh' 
+    # gem instal shotgun  autoload the script instead of restart every time where modify something
+    # shotgun sinatra.rb
 end
 
 get '/hello/:name' do
@@ -13,5 +16,5 @@ get '/hello/:name' do
 end
 
 get '/bird/:sex' do
-    erb :"hello/world"
+    erb :"hello/world", :layout => :post
 end
