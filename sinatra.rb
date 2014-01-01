6f12adec799ebf99cdfa59ab76fb837afcb56b39
silas.xie <xie.tianbao44@gmail.com>
2014-01-01 21:46:57 +0800

    add layout file

--- sinatra.rb
+++ sinatra.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 set :public_folder, File.dirname(__FILE__) + '/public_folder'
+#set :views, File.dirname(__FILE__) + '/templates'
+
 get '/' do
     'hello worldddd!'
 end
@@ -8,4 +10,8 @@ get '/hello/:name' do
     @tian = params[:name]
     @bao = 'HHHHHHHHHHHHHHHHHHHHHHHHhh'
     erb :hello
+end
+
+get '/bird/:sex' do
+    erb :"hello/world"
 end
