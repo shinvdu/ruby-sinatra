947dcea71b44ab7c057af9481903554790442a86
silas.xie <silas.xie@vidaxl.com>
2014-01-03 10:05:22 +0800

    a full crud example

--- sing_with_sinatra/recall.rb
+++ sing_with_sinatra/recall.rb
@@ -31,6 +31,41 @@ post '/' do
     n.save  
     redirect '/'  
 end  
+
+get '/:id' do  
+    @note = Note.get params[:id]  
+    @title = "Edit note ##{params[:id]}"  
+    erb :edit  
+end  
+
+put '/:id' do  
+    n = Note.get params[:id]  
+    n.content = params[:content]  
+    n.complete = params[:complete] ? 1 : 0  
+    n.updated_at = Time.now  
+    n.save  
+    redirect '/'  
+end  
+
+get '/:id/delete' do  
+    @note = Note.get params[:id]  
+    @title = "Confirm deletion of note ##{params[:id]}"  
+    erb :delete  
+end  
+
+delete '/:id' do  
+    n = Note.get params[:id]  
+    n.destroy  
+    redirect '/'  
+end  
+
+get '/:id/complete' do  
+    n = Note.get params[:id]  
+    n.complete = n.complete ? 0 : 1 # flip it  
+    n.updated_at = Time.now  
+    n.save  
+    redirect '/'  
+end  
 # if no such a controler
 not_found do
     halt 404, 'page not found'
